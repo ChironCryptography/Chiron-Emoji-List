3c398493884bd3b1efa4a99ee59175638cf81b3f
jamieyello <jamieyello@yahoo.com>
2022-07-05 12:54:20 -0400

    Temporary changes

--- Chiron.EmojiListGen/Program.cs
+++ Chiron.EmojiListGen/Program.cs
@@ -6,29 +6,24 @@ namespace Chiron.UnicodeListGen
     public static class Program
     {
         public static async Task Main(string[] args) {
-            //Console.WriteLine("Downloading list of emojis from unicode.org...");
-            //var sequences = await HttpHelper.DownloadFileAsText("https://unicode.org/Public/emoji/14.0/emoji-sequences.txt");
-            //var zwy_sequences = await HttpHelper.DownloadFileAsText("https://unicode.org/Public/emoji/14.0/emoji-zwj-sequences.txt");
-            //var emoji_ordering = await HttpHelper.DownloadFileAsText("https://unicode.org/emoji/charts/emoji-ordering.txt");
+            Console.WriteLine("Downloading list of emojis from unicode.org...");
+            var sequences = await HttpHelper.DownloadFileAsText("https://unicode.org/Public/emoji/14.0/emoji-sequences.txt");
+            var zwy_sequences = await HttpHelper.DownloadFileAsText("https://unicode.org/Public/emoji/14.0/emoji-zwj-sequences.txt");
+            var emoji_ordering = await HttpHelper.DownloadFileAsText("https://unicode.org/emoji/charts/emoji-ordering.txt");
 
-            //Console.WriteLine("Parsing downloaded data...");
-            //var emoji = new Dictionary<string, Unicode>();
-            //UnicodeFile.Parse(sequences, UnicodeFile.FileType.sequences, emoji);
-            //UnicodeFile.Parse(zwy_sequences, UnicodeFile.FileType.zwj_sequences, emoji);
-            //var emoji_l = (from d in emoji select d.Value).ToList();
+            Console.WriteLine("Parsing downloaded data...");
+            var emoji = new Dictionary<string, Unicode>();
+            UnicodeFile.Parse(sequences, UnicodeFile.FileType.sequences, emoji);
+            UnicodeFile.Parse(zwy_sequences, UnicodeFile.FileType.zwj_sequences, emoji);
+            var emoji_l = (from d in emoji select d.Value).ToList();
 
-            //var emoji_ordering_l = UnicodeFile.ParseOrder(emoji_ordering);
+            var emoji_ordering_l = UnicodeFile.ParseOrder(emoji_ordering);
 
-            //Console.WriteLine("Generating cs file...");
-            //var code = CodeGeneration.CSharp.Generate(emoji_l, emoji_ordering_l);
-            //File.WriteAllText("CodeGen.cs", code);
+            Console.WriteLine("Generating cs file...");
+            var code = CodeGeneration.CSharp.Generate(emoji_l, emoji_ordering_l);
+            File.WriteAllText("CodeGen.cs", code);
 
-            //Console.WriteLine("Wrote output to 'CodeGen.cs'.");
-
-            foreach (var s in Chiron.EmojiList.AllOrdered)
-            {
-                Console.WriteLine(s);
-            }
+            Console.WriteLine("Wrote output to 'CodeGen.cs'.");
 
             Console.WriteLine("Done.");
         }
